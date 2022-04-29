0e5ff117732ac1491018191d08fb9f9dbf572704
cobalt88 <vincentteune@gmail.com>
2022-04-29 15:18:43 -0400

    4.29.22
    change port information

--- server.js
+++ server.js
@@ -43,16 +43,17 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
-app.get('/api/animals', (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
   }
-  res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
 });
 
 const { animals } = require('./data/animals');
